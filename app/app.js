6cb35fd41904f0936c60343fc2d9749e9b2dfd02
sherpal8 <sherpalkorotana@icloud.com>
2020-08-16 14:25:33 +0100

    Use webpack to generate stats.json . webpack-bundle-analyzer to visually inspect components

--- app/app.js
+++ app/app.js
@@ -1,4 +1,7 @@
 import "./klondike/scoring";
+import "./klondike/klondike.js";
+import "./klondike/board.js";
+import "./klondike/game.js";
 
 angular.module("solitaire", ["klondike", "ngDraggable"]);
 
