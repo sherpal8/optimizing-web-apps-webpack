d5ea2dab4a4e0fb3dd4b1682d1e5337a8ebcf7ee
sherpal8 <sherpalkorotana@icloud.com>
2020-08-17 12:58:05 +0100

    Set up webpack config file, devServer.contentBase, devServer.publicPath

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+  entry: './app/app.js',
+  output: {
+    path: path.resolve(__dirname, 'app/dist'),
+    filename: 'app.bundle.js'
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'app'),
+    publicPath: '/dist/'
+  },
+};
