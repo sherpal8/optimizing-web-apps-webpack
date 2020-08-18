77a9f0ad5985b82e3cfbe6405b17c1277e2fdd21
sherpal8 <sherpalkorotana@icloud.com>
2020-08-18 10:41:01 +0100

    Set up of environment. Export a function from webpack.config.js to respond to the environment with the correct baseConfig i.e. configuration object

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 
-module.exports = {
+const baseConfig = {
   entry: './app/app.js',
   output: {
     path: path.resolve(__dirname, 'app/dist'),
@@ -15,6 +15,26 @@ module.exports = {
     hotOnly: true
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin()
+    // new webpack.HotModuleReplacementPlugin()
   ]
+};
+
+module.exports = function (env) {
+  /** first, webpack will read the CLI-entered value for ENV */
+  /** then, it will use that value inside this exported function */
+  /** then, it determine the env */
+  /** finally, webpack will return baseConfig object with the right props */
+  const isDevelopment = env === 'development';
+  /**
+  * to set up env, inside CLI type:
+  *   NODE_ENV=development npm run build
+  */
+  console.log(`This is a ${isDevelopment ? 'development' : 'production'} build`);
+
+  if (isDevelopment) {
+    baseConfig.plugins.push(
+      new webpack.HotModuleReplacementPlugin()
+    )
+  };
+   return baseConfig 
 };
